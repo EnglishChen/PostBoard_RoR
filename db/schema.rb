318daef15a278dc7492185b4e65681db624cb598
EnglishChen <s888860398@hotmail.com>
2015-12-21 22:32:12 +0800

    add_comments_table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151221141605) do
+ActiveRecord::Schema.define(version: 20151221142530) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "post_id"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
