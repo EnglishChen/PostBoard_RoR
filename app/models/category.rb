66ca670af12834c8168a6f61ed0b05081e79297b
EnglishChen <s888860398@hotmail.com>
2015-12-26 01:46:24 +0800

    add the beautiful package

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ActiveRecord::Base
 	has_many :post
        has_many :post, through: :categories_posts
+
+         validates :name, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,7 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
   resources :users, only: [:new, :create, :edit, :update, :show]
+  resources :categories, only: [:index, :new, :create]
 
   resources :posts do
     resources :comments
